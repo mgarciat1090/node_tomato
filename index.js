887144d82c3bec1d8ac9d0b1c76b4a879aa62f87
psysonik <psysonik@gmail.com>
2014-01-28 15:14:47 -0600

    read from console

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-var argv = require('optimist').argv;
-console.log(argv);
-for(var i = 0; i < argv.times; i++){
-    console.log(argv._[0] + ' on loop number ' + (i + 1));
-}
+var rl = require('readline');
+
+var prompts = rl.createInterface(process.stdin, process.stdout);
+
+prompts.question("How many servings of fruits and vegetables do you eat each day",function(servings){
+    var message = '';
+
+    if(servings < 5){
+        message = "Since you're only eating " + servings +
+        " right now, you might want to start eating " +  (5 - servings) + " more.";
+    }else{
+        message = "Excellent, your diet is on the right track";
+    }
+    console.log(message);
+    process.exit();
+})
